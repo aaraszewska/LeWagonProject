e38687ea5bdfc5a4eb272a3aca0978ef18808e1e
Anna <aaraszewska@gmail.com>
2025-01-31 12:08:17 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,8 @@ st.title("Cryptocurrency Price Prediction Using ARIMA and Random Forest Models")
 df = pd.read_csv('BTC_10_YEAR.csv')
 st.dataframe(df.head())
 
-df['Date'] = pd.to_datetime(df['Date'])
+
+df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
 
 # Sort by date to ensure correct time series visualization
 pd = pd.sort_values(by='Date')
