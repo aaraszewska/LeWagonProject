8fbecb84d35235b229c52085552d2338f77fc7ed
Anna <aaraszewska@gmail.com>
2025-01-31 11:58:57 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,12 @@ st.title("Cryptocurrency Price Prediction Using ARIMA and Random Forest Models")
 df = pd.read_csv('BTC_10_YEAR.csv')
 st.dataframe(df.head())
 
-# Create the plot
-fig = px.line(bitcoin, x='Date', y='Close', title='Bitcoin Price Over Time', labels={'Close': 'Price (USD)'})
+df['Date'] = pd.to_datetime(df['Date'])
+
+# Sort by date to ensure correct time series visualization
+pd = pd.sort_values(by='Date')
+fig = px.histogram(df,  x='Date', y='Close', title='Bitcoin Price Over Time', labels={'Close': 'Price (USD)'})
 
 # Display the plot in Streamlit
 st.plotly_chart(fig)
+)
