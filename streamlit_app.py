195772d39cc39fc795054e5c41cd0ea27e4df011
Anna <aaraszewska@gmail.com>
2025-01-31 12:16:59 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,15 +8,7 @@ st.title("Cryptocurrency Price Prediction Using ARIMA and Random Forest Models")
 
 df = pd.read_csv('BTC_10_YEAR.csv')
 st.dataframe(df.head())
-df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
-df['Close'] = pd.to_numeric(df['Close'], errors='coerce')
+fig = px.histogram(df, x='Date', y= 'Close')
+st.ploty_chart(fig)
 
-# Drop rows with missing values in 'Date' or 'Close'
-df = df.dropna(subset=['Date', 'Close'])
-
-# Create the histogram plot
-fig = px.histogram(df, x='Date', y='Close', title='Bitcoin Price Over Time', labels={'Close': 'Price (USD)'})
-
-# Show the plot
-fig.show()
 
