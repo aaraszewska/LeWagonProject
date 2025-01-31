d136e3d505e797abd7cb072e26f33345d0192d29
Anna <aaraszewska@gmail.com>
2025-01-31 12:10:41 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,14 +9,14 @@ st.title("Cryptocurrency Price Prediction Using ARIMA and Random Forest Models")
 df = pd.read_csv('BTC_10_YEAR.csv')
 st.dataframe(df.head())
 
-
+# Assuming 'df' is your DataFrame
 df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
 
+# Sort by 'Date' column and assign it back to 'df'
+df = df.sort_values(by='Date')
 
+# Continue with your Plotly or other operations
 
-
-# Sort by date to ensure correct time series visualization
-pd = pd.sort_values(by='Date')
 fig = px.histogram(df,  x='Date', y='Close', title='Bitcoin Price Over Time', labels={'Close': 'Price (USD)'})
 
 # Display the plot in Streamlit
