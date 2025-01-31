b2875cf2d1dfad45918a7da31efd78dafc296f9c
Anna <aaraszewska@gmail.com>
2025-01-31 12:19:22 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,11 @@ st.title("Cryptocurrency Price Prediction Using ARIMA and Random Forest Models")
 
 df = pd.read_csv('BTC_10_YEAR.csv')
 st.dataframe(df.head())
-fig = px.histogram(df, x='Date', y= 'Close')
+df.columns = df.columns.str.strip()  # Strips any leading or trailing spaces
+
+
+fig = px.histogram(df, x='Date', y='Close', title='Bitcoin Price Over Time', labels={'Close': 'Price (USD)'})
+
 st.ploty_chart(fig)
 
 
