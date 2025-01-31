9d03a0f5a677a54e5e824e27fca5b904bf43e305
Anna <aaraszewska@gmail.com>
2025-01-31 15:48:45 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,14 @@ import plotly.express as px
 
 #requirements.txt
 st.title("Cryptocurrency Price Prediction Using ARIMA and Random Forest Models")
+mport streamlit as st
 
+create_page = st.Page("create.py", title="Create entry", icon=":material/add_circle:")
+delete_page = st.Page("delete.py", title="Delete entry", icon=":material/delete:")
+
+pg = st.navigation([create_page, delete_page])
+st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
+pg.run()
 
 df = pd.read_csv('BTC_10_YEAR.csv')
 st.dataframe(df.head())
