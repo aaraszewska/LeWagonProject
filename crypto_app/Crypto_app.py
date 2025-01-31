2f1877ddf4bc17dcec6d19e0961f8ac3675723c8
Anna <aaraszewska@gmail.com>
2025-01-31 16:48:25 +0000

    Update Crypto_app.py

--- crypto_app/Crypto_app.py
+++ crypto_app/Crypto_app.py
@@ -1,12 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="Crypto Dashboard", page_icon="💰", layout="wide")
-
-st.sidebar.title("Navigation")
-st.sidebar.page_link("crypto_app/pages/home.py", label="🏠 Home")
-
-st.title("Cryptocurrency Price Prediction")
-st.write("Welcome to the Crypto Dashboard. Navigate using the sidebar.")
+st.sidebar.page_link("pages/home.py", label="🏠 Home")  # No need for "crypto_app/" in the path
 
 
 
