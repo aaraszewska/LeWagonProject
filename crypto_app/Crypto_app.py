3780446c7ff79f175ad3d7a8c439e18bde0bd80e
Anna <aaraszewska@gmail.com>
2025-01-31 16:24:01 +0000

    Update Crypto_app.py

--- crypto_app/Crypto_app.py
+++ crypto_app/Crypto_app.py
@@ -1,8 +1,4 @@
 import streamlit as st
-import pandas as pd
-import plotly.express as px
-
-import streamlit as st
 
 st.set_page_config(page_title="Crypto Dashboard", page_icon="💰", layout="wide")
 
