61c06aa96e5d7e0c32a38fa88af4825f1bd0e87a
Anna <aaraszewska@gmail.com>
2025-01-31 16:38:12 +0000

    Update Crypto_app.py

--- crypto_app/Crypto_app.py
+++ crypto_app/Crypto_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(page_title="Crypto Dashboard", page_icon="💰", layout="wide")
 
 st.sidebar.title("Navigation")
-st.sidebar.page_link("pages/home.py", label="🏠 Home")
+st.sidebar.page_link("crypto_app/pages/home.py", label="🏠 Home")
 
 st.title("Cryptocurrency Price Prediction")
 st.write("Welcome to the Crypto Dashboard. Navigate using the sidebar.")
@@ -11,4 +11,4 @@ st.write("Welcome to the Crypto Dashboard. Navigate using the sidebar.")
 
 
 
-st.sidebar.page_link("crypto_app/pages/home.py", label="🏠 Home")
+
